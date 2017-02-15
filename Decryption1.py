e8afb72f30f7bbe1f37880aa07d58fb0e431ba0d
TheGrayGoat <davidglhs@gmail.com>
2017-02-14 21:09:57 -0600

    Update Decryption1.py

--- Decryption1.py
+++ Decryption1.py
@@ -1,5 +1,4 @@
 from cryptography.fernet import Fernet
-from Encryption1 import cipher_text, key
-cipher_suite = Fernet(key)
-plain_text = cipher_suite.decrypt(cipher_text)
+cipher_suite = Fernet(#The key goes here)
+plain_text = cipher_suite.decrypt(#The cipher_text goes here)
 print(plain_text)
